533fc45beccea9703080c4f51f0071a59672ae6c
SailendraDarbha94 <sailendra.darbha@gmail.com>
2025-02-17 14:58:04 +0530

    reset defaults

--- lib/supabase/browser.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { Database } from "../types/supabase";
-
-
-export const supabaseBrowser = () => createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-);
